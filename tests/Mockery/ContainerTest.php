d2a0e7e4e90c9d92be654c2f13fc3f3ca007919a
Steven Rosato <steven.rosato@majisti.com>
2013-08-16 22:19:40 -0400

    Added another test to test against IteratorAggregate.

--- tests/Mockery/ContainerTest.php
+++ tests/Mockery/ContainerTest.php
@@ -834,6 +834,14 @@ class ContainerTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Traversable', $mock);
     }
 
+    public function testMockingInterfaceThatExtendsIteratorAggregateDoesNotImplementIterator()
+    {
+        $mock = $this->container->mock('MockeryTest_InterfaceThatExtendsIteratorAggregate');
+        $this->assertInstanceOf('IteratorAggregate', $mock);
+        $this->assertInstanceOf('Traversable', $mock);
+        $this->assertNotInstanceOf('Iterator', $mock);
+    }
+
     public function testMockingIteratorAggregateDoesNotImplementIteratorAlongside()
     {
         $mock = $this->container->mock('IteratorAggregate');
@@ -1096,3 +1104,8 @@ class MockeryTest_ImplementsIterator implements \Iterator {
 interface MockeryTest_InterfaceThatExtendsIterator extends \Iterator {
     public function foo();
 }
+
+interface MockeryTest_InterfaceThatExtendsIteratorAggregate extends \IteratorAggregate {
+    public function foo();
+}
+
